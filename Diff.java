d0a3d37338750bb3ecaa61023312951fc3dc19db
Kachwal Uttam Sharma <kachwaluttamsharma@gmail.com>
2021-08-16 12:02:39 +0530

    added multiply

--- Diff.java
+++ Diff.java
@@ -1,3 +1,7 @@
-public class Diff {
-    
+public class Diff {  
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        System.out.println("The difference is " + (a-b));
+}
 }
